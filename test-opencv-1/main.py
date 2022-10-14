39dd62c75844160a5510d96ab12fe73fc94a956a
majo48 <mart.jonasse@gmail.com>
2022-10-14 16:44:43 +0200

    first steps

---
+++ test-opencv-1/main.py
@@ -0,0 +1,11 @@
+# this is the first of 'learning by doing' packages
+
+def main():
+    print('This is the main test package #1.')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
