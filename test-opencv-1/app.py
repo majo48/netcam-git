b419d16a4f8abaaa89ab9e57db10fa4cdd3603ed
majo48 <mart.jonasse@gmail.com>
2022-10-14 19:42:59 +0200

    Refactored

--- test-opencv-1/app.py
+++ test-opencv-1/app.py
@@ -1,11 +1,11 @@
 # this is the first of 'learning by doing' packages
 
-def main():
+def run():
     print('This is the main test package #1.')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main()
+    run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
