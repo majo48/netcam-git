28b41d16762fed8ab89b8fe6e14009624daf0047
majo48 <mart.jonasse@gmail.com>
2022-10-31 14:17:35 +0100

    Added test-python-1 Test code for overlapping rectangles in the opencv coordinate system.

---
+++ test-python-1/app.py
@@ -0,0 +1,35 @@
+# proof of concept
+def intersect(boxA, boxB):
+    """
+        check if two rectangles intersect, using opencv coordinates
+        e.g. top left is [0, 0], both x and y ascending (>0)
+        this may return an empty area (with w * h == 0)
+    """
+    x = max(boxA["x"], boxB["x"])
+    y = max(boxA["y"], boxB["y"])
+    w = min(boxA["x"] + boxA["w"], boxB["x"] + boxB["w"]) - x
+    h = min(boxA["y"] + boxA["h"], boxB["y"] + boxB["h"]) - y
+    intersects = True
+    if w < 0 or h < 0:
+        intersects = False
+    return intersects, [x, y, w, h]
+
+
+# test code
+if __name__ == "__main__":
+    boxes = [
+        {"x": 0, "y": 0, "w": 10, "h": 10, "name": "box1"},
+        {"x": 5, "y": 5, "w": 10, "h": 10, "name": "box2"},
+        {"x": 20, "y": 20, "w": 10, "h": 10, "name": "box3"},
+        {"x": 10, "y": 10, "w": 10, "h": 10, "name": "box4"}
+    ]
+    for i in range(len(boxes)):
+        boxA = boxes[i]
+        for j in range(i+1, len(boxes)):
+            boxB = boxes[j]
+            intersects, rectangle = intersect(boxA, boxB)
+            if intersects:
+                ret = 'intersects'
+            else:
+                ret = 'none intersecting'
+            print("Compare "+boxA["name"]+" with "+boxB["name"]+": "+ret+" "+str(rectangle))
