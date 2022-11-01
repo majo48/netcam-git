8f28a56d9ba03b5b2e84f35647bc66c763416ce3
majo48 <mart.jonasse@gmail.com>
2022-11-01 16:13:45 +0100

    Update test-opencv-3 Implment intersects in the motions.py file.

--- test-opencv-3/motions.py
+++ test-opencv-3/motions.py
@@ -5,11 +5,13 @@
 import cv2
 import numpy as np
 import os
+from operator import itemgetter
 
 class Motions:
     """ detect motions in video stream frames """
 
     BG_SUB_METHODE = 'MOG2' # backgrounde subtraction methode: ( 'MOG2', 'KNN')
+    FG_MIN_AREA = 500 # minimal size of green boxes
 
     def __init__(self, vidObj):
         """ create instance of motions """
@@ -30,8 +32,9 @@ class Motions:
         pass
 
     def parse_frame(self, frame):
-        """ parse one frame in the video stream """
-
+        """
+            parse one frame in the video stream
+        """
         # update the background model
         fgMask = self.backSub.apply(frame)
         cv2.imshow('FG Mask', fgMask)
@@ -44,14 +47,79 @@ class Motions:
         # check for motions
         motionDetected = False
         contours, hierarchy = cv2.findContours(fgMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        minArea = (500)
+        boxes = self._merge_overlaps(contours, frame)
+        for box in boxes:
+            if box["valid"]:
+                motionDetected = True
+                cv2.rectangle(frame, (box['x'], box['y']), (box['x2'], box['y2']), (0, 255, 0), 4)
+            else:
+                dummy = 'stopq'
+        # finished
+        return motionDetected, frame
+
+    def _merge_overlaps(self, contours, frame=None):
+        """
+            merge all overlaps in the contours list
+        """
+        # build list of boxes fro, the contours
+        boxes = []
         for i in range(len(contours)):
             x, y, w, h = cv2.boundingRect(contours[i])
-            if (w * h) > minArea:
-                motionDetected = True
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            area = w * h # pixels
+            if area > self.FG_MIN_AREA:
+                boxes.append({
+                    'x': x, 'y': y, 'w': w, 'h': h,
+                    'x2': x + w, 'y2': y + h, 'area': area,
+                    'order': i, 'valid': True })
+                # display the contours in red
+                # if frame is not None:
+                #     cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 10)
 
-        return motionDetected, frame
+        # check for overlaps
+        for i in range(len(boxes)):
+            box1 = boxes[i]
+            if box1["valid"]:
+                for j in range(i+1, len(boxes)):
+                    box2 = boxes[j]
+                    if box2["valid"]:
+                        intersects, area = self._intersecting_boxes(box1, box2)
+                        if intersects:
+                            boxes[i], boxes[j] = self._merge_boxes(box1, box2)
+        # finished
+        return boxes
+
+    def _intersecting_boxes(self, boxA, boxB):
+        """
+            Check if two rectangles intersect, using opencv coordinates,
+            defined as: top left is [0, 0], both x and y ascending (>0).
+            This can return a positive result, with an empty area (w*h == 0).
+        """
+        x = max(boxA["x"], boxB["x"])
+        y = max(boxA["y"], boxB["y"])
+        w = min(boxA["x"] + boxA["w"], boxB["x"] + boxB["w"]) - x
+        h = min(boxA["y"] + boxA["h"], boxB["y"] + boxB["h"]) - y
+        intersects = True
+        if w < 0 or h < 0:
+            intersects = False
+        return intersects, [x, y, w, h]
+
+    def _merge_boxes(self, box1, box2):
+        """"
+            merge box2 into box1
+        """
+        # recalculate borders and size of box1
+        box1["x"] = min(box1["x"], box2["x"]) # leftmost value
+        box1["y"] = min(box1["y"], box2["y"]) # topmost value
+        box1["x2"] = max(box1["x2"], box2["x2"]) # rightmost value
+        box1["y2"] = max(box1["y2"], box2["y2"]) # bottommost value
+        box1["w"] = box1["x2"] - box1["x"]
+        box1["h"] = box1["y2"] - box1["y"]
+        box1["area"] = box1["w"] * box1["h"]
+        # cancel box2
+        box2["valid"] = False
+        # finished
+        return box1, box2
+
 
 if __name__ == '__main__':
     print(
