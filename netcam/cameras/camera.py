9d4adef75ed5bdbdb2084119f0fc720f5f1a0e84
majo48 <mart.jonasse@gmail.com>
2023-01-17 17:20:57 +0100

    Added multithreading Displays videoclip on home page. Issue: static picture, not moving!

---
+++ netcam/cameras/camera.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2022 Martin Jonasse, Zug, Switzerland
+
+import cv2
+import queue
+from imutils.video import VideoStream
+import os
+import threading
+import logging
+
+
+class Camera(threading.Thread):
+    """ class for one physical video cameras """
+
+    def __init__(self, idx, rtsp_url):
+        """
+        initialize connection to one physical video camera
+        """
+        threading.Thread.__init__(self)
+        self.idx = idx # thread index
+        self.rtsp_url = rtsp_url # url of the external camera
+        self.frame_count = 0 # frame count
+        self.skipped_frames = 0 # counts the number of frames skipped
+        self.qq = queue.Queue(maxsize=1) # interface to other threads
+
+    def run(self):
+        """
+        open/connect video stream from one physical video camera
+        """
+        thread = threading.currentThread()
+        vs = VideoStream(self.rtsp_url).start()
+        logging.info("Started video stream in thread '" + thread.name + "'\n")
+        # loop through all frames provided by the camera stream
+        while True:
+            # get frames from camera (blocking)
+            frame = vs.read()
+            # check frame
+            if frame is None:
+                # todo: add some proper error handling here
+                continue
+            # increment frame count
+            self.frame_count += 1
+            # put one frame in the queue
+            try:
+                self.qq.put(frame, block=False)
+            except queue.Full:
+                # queue already has a frame (not consumed)
+                self.skipped_frames += 1
+            pass # keep on looping
+
+        cv2.destroyAllWindows()
+        vs.stop()
+
+    def get_frame_count(self):
+        """ get the frame count """
+        return self.frame_count
+
+    def get_frame(self):
+        """ get frame (with blocking) """
+        return self.qq.get(block=True, timeout=None)
+
+
+if __name__ == '__main__':
+    print(
+        'So sorry, the ' +
+        os.path.basename(__file__) +
+        ' module does not run as a standalone.')
--- netcam/netcam.py
+++ netcam/netcam.py
@@ -3,18 +3,31 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from flask import make_response
+import cv2
+
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
+    idx=0
     return render_template(
         template_name_or_list='home.html',
         navigation={
             "icon": "hamburger",
-            "url": url_for("menu_main", _external=True) }
+            "url": url_for("menu_main", _external=True) },
+        index=idx
     )
+
+@app.route("/video_feed/<idx>")
+def video_feed(idx):
+    frm = thrds[int(idx)].get_frame() # blocking
+    retval, buffer = cv2.imencode('.png', frm)
+    response = make_response(buffer.tobytes())
+    response.headers['Content-Type'] = 'image/png'
+    return response
 
 @app.route("/menu/main")
 def menu_main():
@@ -86,9 +99,33 @@ def logs():
             "url": url_for("home", _external=True) }
     )
 
+def setup_threads():
+    """ setup all threads needed for this app """
+    from cameras import config
+    conf = config.Config()
+    ips = conf.get_ip_adresse_list()
+    from cameras import camera
+    cams = []
+    for idx, ip in enumerate(ips):
+        url = conf.get_rtsp_url(idx)
+        cam = camera.Camera(idx,url)
+        cam.daemon = True
+        cam.start()
+        cams.append(cam)
+    return cams
+
 
 if __name__ == "__main__":
-    # run in development environment only, no external access possible (safe)
-    app.run(debug=True, use_reloader=False)
-    # run on all IP addresses, external access allowed (unsafe)
+    """ initialize the netcam app """
+    # setup all threads needed for the application
+    thrds = setup_threads()
+
+    # [safe] run Flask webserver in development environment only, no external access possible (safe)
+    app.run(debug=True, use_reloader=False) # or comment + uncomment the last line of code
+
+    # [unsafe] run on all IP addresses, external access allowed
     # app.run(debug=True, use_reloader=False, host='0.0.0.0', port=5000)
+
+    # kill threads
+    for thrd in thrds:
+        thrd.join()
